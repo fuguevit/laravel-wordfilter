24f0a451989e1ebbe8d03e3cd93cf1c0709ecd64
fugue <wangxuan@188.com>
2016-11-16 23:42:41 +0800

    add more test

--- tests/WordFilterTest.php
+++ tests/WordFilterTest.php
@@ -11,8 +11,12 @@ class WordFilterTest extends TestCase
         $arr_badwords = ['六四', '江泽民', '李鹏'];
 
         $result = str_contains($string, $arr_badwords);
+        $this->assertTrue($result);
 
-        return $this->assertTrue($result);
+        $collection = collect([['江泽民', 2, 3], [4, 5, 6], [7, 8, 9]]);
+
+        $result2 = str_contains('OK江泽民觉得六四好', $collection->collapse()->all());
+        $this->assertTrue($result2);
     }
 
 }
