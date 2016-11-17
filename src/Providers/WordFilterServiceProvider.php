7629102836722471b47448781bf820ea062a43b2
wangxuan <wangxuan@qgpic.com>
2016-11-17 11:19:55 +0800

    add service provider; change package name.

---
+++ src/Providers/WordFilterServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Fuguevit\WordFilter\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WordFilterServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->publishes([
+            realpath(__DIR__ . '/../../database/migrations/') => database_path('migrations'),
+        ], 'migrations');
+
+        $this->publishes([
+            realpath(__DIR__ . '/../../config/word.filter.php') => config_path('word.filter.php'),
+        ], 'config');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(realpath(__DIR__ . '/../../config/word.filter.php'), 'word.filter');
+    }
+}
