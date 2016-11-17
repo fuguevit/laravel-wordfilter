6c9ca8908ea5431be1453172bd0c6b805d68ad8e
fugue <wangxuan@188.com>
2016-11-17 23:52:02 +0800

    change test function

--- tests/WordFilterTest.php
+++ tests/WordFilterTest.php
@@ -4,19 +4,14 @@ namespace Fuguevit\WordFilter\Tests;
 
 class WordFilterTest extends TestCase
 {
-    public function test_stub()
+    public function test_bad_words()
     {
-        $string = "OK我觉得六四好";
+        $string = "OK我觉得五三好";
 
-        $arr_badwords = ['六四', '江泽民', '李鹏'];
+        $arr_badwords = ['五三'];
 
         $result = str_contains($string, $arr_badwords);
         $this->assertTrue($result);
-
-        $collection = collect([['江泽民', 2, 3], [4, 5, 6], [7, 8, 9]]);
-
-        $result2 = str_contains('OK江泽民觉得六四好', $collection->collapse()->all());
-        $this->assertTrue($result2);
     }
 
 }
